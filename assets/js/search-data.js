1a813f3d3029f110cc5586c0124497c08d6f10d3
HangShuai523 <64656762+HangShuai523@users.noreply.github.com>
2025-02-07 22:31:39 +0000

    Deploying to gh-pages from @ HangShuai523/HangShuai523.github.io@f8c0ade497a775339c616f6a3cdf33684b644c18 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -439,8 +439,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-paper-hydroclimate-coupled-framework-for-assessing-power-system-resilience-under-summer-drought-and-climate-change-was-accepted-by-renewable-and-sustainable-energy-review",
+          title: 'Our paper “Hydroclimate-coupled framework for assessing power system resilience under summer drought and...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
